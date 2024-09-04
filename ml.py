ca08b2864069bcac6dca6d960fe607770ff04ec1
mahathi99 <relangimahathi.22.csm@anits.edu.in>
2024-09-04 11:56:55 +0530

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Hello Streamlit!")
+st.write("This is a simple Streamlit app with just a few lines of code.")
+st.write("Streamlit makes it easy to create interactive web apps for machine learning.")
